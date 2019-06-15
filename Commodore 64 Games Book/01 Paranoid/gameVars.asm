fd0afb872700de17c81d4273830dc43a3a0f56d2
markstilton <49697384+markstilton@users.noreply.github.com>
2019-06-15 19:48:49 +0100

    Create gameVars.asm

---
+++ Commodore 64 Games Book/01 Paranoid/gameVars.asm
@@ -0,0 +1,16 @@
+;game variables
+xPos            word    $00
+yPos            word    $00
+x1Pos           word    $00
+y1Pos           word    $00
+dx              byte    $00
+dy              byte    $00
+xA              byte    $00
+xB              byte    $00
+xC              byte    $00
+yA              byte    $00
+yB              byte    $00
+pPos            byte    $00, $00
+gPArray         byte    $80, $40, $20, $10, $08, $04, $02, $01
+xConst          byte    $3F, $01
+yConst          byte    $C7, $00
