b7d45e253bc246635acc8d70273654024cbcfaa5
markstilton <49697384+markstilton@users.noreply.github.com>
2019-06-15 19:46:35 +0100

    Create gameMain.asm

---
+++ Commodore 64 Games Book/01 Paranoid/gameMain.asm
@@ -0,0 +1,15 @@
+*=$0801
+
+        byte    $0E, $08, $0A, $00, $9E, $20, $28
+        byte    $32, $30, $36, $34, $29, $00, $00, $00
+
+Start
+        jsr Initialise
+
+GameLoop
+
+        jsr PlotPointsXY
+        jsr CheckXYBoundaries
+        jsr CalculateDXDY
+
+        jmp GameLoop
